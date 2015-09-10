7ceaf9b3088e72cf01411d24e4b549b6b1aeb25b
Alexander Denisov <stonespb@yandex-team.ru>
2015-09-10 10:46:43 +0300

    Delta bar caps.

--- deltabar/deltabar_lib/deltabar_lib.py
+++ deltabar/deltabar_lib/deltabar_lib.py
@@ -1,5 +1,6 @@
 import ac
 import acsys
+import math
 import sys
 import time
 
@@ -29,6 +30,25 @@ def getTrack():
     track = '{}-{}'.format(track, track_config)
 
   return track
+
+
+def sign(x):
+  return math.copysign(1, x)
+
+
+def renderPolygon(p1, p2, p3, p4, reversedNormal):
+  ac.glBegin(3)
+  if not reversedNormal:
+    ac.glVertex2f(*p1)
+    ac.glVertex2f(*p2)
+    ac.glVertex2f(*p3)
+    ac.glVertex2f(*p4)
+  else:
+    ac.glVertex2f(*p1)
+    ac.glVertex2f(*p4)
+    ac.glVertex2f(*p3)
+    ac.glVertex2f(*p2)
+  ac.glEnd()
 
 
 class LabelTracker:
@@ -530,44 +550,134 @@ class Delta:
       ac.setText(self.data.banner_label, "")
 
   def draw_delta_bar(self, time_delta, speed_delta):
-    bar_color = self.delta_bar_color(speed_delta)
+    # Color
+    bar_color = self.delta_stripe_color(speed_delta)
+    ac.glColor4f(*bar_color)
 
-    clamped_time_delta = self.clamp_delta_time(time_delta)
+    # Calculations
+    clamped_time_delta = self.clamp_time_delta(time_delta)
     delta_stripe_width = int((abs(clamped_time_delta) * config.BAR_SCALE))
+    delta_stripe_rect_width = delta_stripe_width
+    should_draw_cap = delta_stripe_width > config.BAR_INNER_RECT_MAX_WIDTH
+
     if time_delta < 0:
-      offset = config.BAR_WIDTH_HALF
-      delta_label_position = offset + delta_stripe_width
+      delta_rect_origin_x = config.BAR_WIDTH_HALF
+      delta_label_position = delta_rect_origin_x + delta_stripe_width
     else:
-      offset = config.BAR_WIDTH_HALF - delta_stripe_width
-      delta_label_position = offset
+      delta_rect_origin_x = config.BAR_WIDTH_HALF - delta_stripe_width
+      delta_label_position = delta_rect_origin_x
 
+    # Draw rounded cap and modify width and origin
+    if should_draw_cap:
+      delta_stripe_rect_width = config.BAR_INNER_RECT_MAX_WIDTH
+      if time_delta > 0:
+        delta_rect_origin_x = config.BAR_WIDTH_HALF - delta_stripe_rect_width
+
+      self.draw_delta_stripe_cap(time_delta, delta_rect_origin_x, delta_stripe_width)
+
+    # Draw stripe rect
     if delta_stripe_width > 0:
-      ac.glColor4f(*bar_color)
-      ac.glQuad(offset, config.BAR_INNER_Y, delta_stripe_width, config.BAR_INNER_HEIGHT)
+      ac.glQuad(delta_rect_origin_x, config.BAR_INNER_Y, delta_stripe_rect_width, config.BAR_INNER_HEIGHT)
 
+    # Delta label
     self.update_delta_label(time_delta, delta_label_position)
 
-  def delta_bar_color(self, speed_delta):
+  def delta_stripe_color(self, speed_delta):
+    """
+    Calculates delta stripe color according to speed delta value.
+    :param speed_delta: speed delta value
+    :return: calculated color
+    """
     # NOTE: Scale from 0.0 meters/sec = 1.0  to  5.0 meters/sec = 0.0
     #       If you change 5.0, change 0.2 to 1/new_value
     x = 1.0 - min(abs(speed_delta), 5.0) * 0.2
     if speed_delta >= 0.0:
       red = 0.1 + 0.9 * x
-      blue = 0.1 + 0.9 * x ** 1.25
+      blue = 0.1 + 0.9 * math.pow(x, 1.25)
       return (red, 1.0, blue, 1.0)  # From green to white
     else:
       green = x
-      blue = x ** 2.0
+      blue = math.pow(x, 2.0)
       return (1.0, green, blue, 1.0)  # From red to white
 
-  def clamp_delta_time(self, time_delta):
+  def clamp_time_delta(self, time_delta):
+    """
+    Clamps time delta absolute value to 2 seconds.
+    :param time_delta: time delta value
+    :return: clamped value
+    """
     if time_delta > 2000:
       return 2000
     elif time_delta < -2000:
       return -2000
     return time_delta
 
+  def draw_delta_stripe_cap(self, time_delta, delta_rect_origin_x, delta_stripe_width):
+    """
+    Draws left or right cap for delta bar stripe
+    :param time_delta: time delta value
+    :param delta_rect_origin_x: x coordinate of delta stripe's rectangular part
+    :param delta_stripe_width: full delta stripe width
+    """
+    cap_origin_x = delta_rect_origin_x
+    if time_delta < 0:
+      cap_origin_x += config.BAR_INNER_RECT_MAX_WIDTH
+    cap_width = - sign(time_delta) * (delta_stripe_width - config.BAR_INNER_RECT_MAX_WIDTH)
+
+    self.draw_horizontal_cap(cap_origin_x,
+                             config.BAR_INNER_Y,
+                             cap_width,
+                             config.BAR_INNER_HEIGHT,
+                             config.BAR_INNER_CORNER_RADIUS,
+                             config.BAR_INNER_CORNER_SEGMENTS)
+
+  def draw_horizontal_cap(self, origin_x, origin_y, width, height, corner_radius, corner_segments):
+    """
+    Draws left or right cap for rectangle with rounded corners starting from straight border.
+    :param origin_x: x coordinate of straight border
+    :param origin_y: y coordinate of figure
+    :param width: width of figure. The value < 0 means that cap is oriented leftwards.
+      If width is less than corner radius - the cap is truncated
+    :param height: height of the figure
+    :param corner_radius: radius of cap corner
+    :param corner_segments: number of segments in cap corner
+    """
+    reversed_normal_of_poly = width < 0  # Don't reorder points - just mark as reversed polygon's normal
+
+    bottom_y = origin_y + height
+    segment_angle = math.pi / (2 * corner_segments)
+    last_segment_x = 0
+    last_segment_y = 0
+    completed = False
+
+    for i in range(1, corner_segments + 1):
+      if completed:
+        break
+
+      angle = math.pi / 2 - i * segment_angle
+      segment_x = sign(width) * math.cos(angle) * corner_radius
+      if abs(width) <= abs(segment_x):
+        completed = True
+        segment_x = width
+        angle = math.acos(segment_x / corner_radius)
+
+      segment_y = corner_radius * (1 - math.sin(angle))
+
+      renderPolygon((origin_x + last_segment_x, origin_y + last_segment_y),
+                    (origin_x + last_segment_x, bottom_y - last_segment_y),
+                    (origin_x + segment_x, bottom_y - segment_y),
+                    (origin_x + segment_x, origin_y + segment_y),
+                    reversed_normal_of_poly)
+
+      last_segment_x = segment_x
+      last_segment_y = segment_y
+
   def update_delta_label(self, time_delta, position):
+    """
+    Updates delta label with new value if needed.
+    :param time_delta: time delta value
+    :param position: x coordinate of label center
+    """
     plus = '-' if time_delta < 0 else '+'
     # NOTE: The below .format()s are too much magic.
     #       We want 234 to be '0.23' and 12345 to be '12.34'
@@ -593,6 +703,11 @@ class Delta:
     ac.setText(self.data.delta_label, label_text)
 
   def delta_label_color(self, time_delta):
+    """
+    Calculates delta label color according to time delta value.
+    :param time_delta: time delta value
+    :return: calculated color
+    """
     if time_delta < 0:
       return GREEN_COLOR
     else:
